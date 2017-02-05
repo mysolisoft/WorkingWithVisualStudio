ebacdd4bf9d7cbfd5d147ce0b1aa1b752390e152
mysolisoft <lp_yl@163.com>
2017-02-05 22:47:36 +0800

    commit all modification

---
+++ src/WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WorkingWithVisualStudio.Models;
+using System.Linq;
+
+namespace WorkingWithVisualStudio.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+            => View(SimpleRepository.SharedRepository.Products
+                .Where(i=>i?.Price<50));
+    }
+}

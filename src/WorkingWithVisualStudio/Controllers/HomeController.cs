1064c5a42b5dc2794368ba0b2b6d038b6b25a391
mysolisoft <lp_yl@163.com>
2017-02-06 21:34:30 +0800

    practise using bundler&minify css and js in visual studio commity

--- src/WorkingWithVisualStudio/Controllers/HomeController.cs
+++ src/WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -8,10 +8,18 @@ using System.Linq;
 
 namespace WorkingWithVisualStudio.Controllers
 {
-    public class HomeController:Controller
+    public class HomeController : Controller
     {
-        public IActionResult Index()
-            => View(SimpleRepository.SharedRepository.Products
-                .Where(i=>i?.Price<50));
+        SimpleRepository Repository = SimpleRepository.SharedRepository;
+        public IActionResult Index() => View(Repository.Products
+        .Where(p => p?.Price < 50));
+        [HttpGet]
+        public IActionResult AddProduct() => View(new Product());
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            Repository.AddProduct(p);
+            return RedirectToAction("Index");
+        }
     }
 }
